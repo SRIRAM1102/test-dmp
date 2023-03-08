04dc51609e425cf1119d6824cf5396522592c9da
SRIRAM <85927573+SRIRAM1102@users.noreply.github.com>
2023-03-08 12:46:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,8 @@ const router = new Router();
 router.get('/redirect', async (ctx) => {
   ctx.status = 301;
   redirection_url = `https://www.google.com/dw_uuid=3212`
-  ctx.redirect('https://www.google.com/');
+  ctx.redirect('https://www.vizibl.ai');
 });
-
-router.get('/', async (ctx) => {
-  ctx.body = "fwfew";
-  console.log("erfere")
-});
-
 
 app.use(router.routes());
 
