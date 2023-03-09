088218e96830e15100e9b02b32ea08a9f17f52c1
SRIRAM <85927573+SRIRAM1102@users.noreply.github.com>
2023-03-09 16:05:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-router.get('/redirect', async (ctx) => {
+router.get('/fredirect', async (ctx) => {
   ctx.status = 301;
-  redirection_url = `https://www.google.com/dw_uuid=3212`
+  redirection_url = `https://play.google.com/store/apps/details?id=air.com.ace2three.mobile&hl=en_US&dw_uuid=3433`
+  ctx.redirect('https://www.vizibl.ai');
+});
+
+router.get('/gredirect', async (ctx) => {
+  ctx.status = 301;
+  redirection_url = `https://play.google.com/store/apps/details?id=com.legendsatwar.strategy&dw_uuid=7896`
   ctx.redirect('https://www.vizibl.ai');
 });
 
