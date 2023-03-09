6166cf14c458b7a0cef430f9692cfcf131e2abcc
SRIRAM <85927573+SRIRAM1102@users.noreply.github.com>
2023-03-09 16:12:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const router = new Router();
 router.get('/fredirect', async (ctx) => {
   ctx.status = 301;
   redirection_url = `https://play.google.com/store/apps/details?id=in.a23.rummy&hl=en_US&dw_uuid=3433`
-  ctx.redirect('https://www.vizibl.ai');
+  ctx.redirect(redirection_url);
 });
 
 router.get('/gredirect', async (ctx) => {
   ctx.status = 301;
   redirection_url = `https://play.google.com/store/apps/details?id=com.legendsatwar.strategy&dw_uuid=7896`
-  ctx.redirect('https://www.vizibl.ai');
+  ctx.redirect(redirection_url);
 });
 
 app.use(router.routes());
